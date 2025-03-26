4415d7d253e9b525c6ffe91e75219fa10f164ad6
Greg-79 <155834378+Greg-79@users.noreply.github.com>
2025-03-26 12:58:40 +0300

    Add files via upload

---
+++ esphome/components/cg_anem/cg_anem.cpp
@@ -0,0 +1,286 @@
+#include "cg_anem.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace cg_anem {
+
+static const char *const TAG = "cg_anem.sensor";
+
+static const uint8_t CG_ANEM_REGISTER_VERSION = 0x04;
+static const uint8_t CG_ANEM_REGISTER_WHO_I_AM = 0x05;
+static const uint8_t CG_ANEM_REGISTER_STATUS = 0x06;
+
+static const uint8_t CG_ANEM_REGISTER_WIND_H = 0x07;
+static const uint8_t CG_ANEM_REGISTER_WIND_L = 0x08; // 16 bit
+
+static const uint8_t CG_ANEM_REGISTER_TEST_COLD_H = 0x09;
+static const uint8_t CG_ANEM_REGISTER_TEST_COLD_L = 0x0A;
+static const uint8_t CG_ANEM_REGISTER_TEST_HOT_H = 0x0B;
+static const uint8_t CG_ANEM_REGISTER_TEST_HOT_L = 0x0C;
+
+static const uint8_t CG_ANEM_REGISTER_VIN = 0x0D;
+static const uint8_t CG_ANEM_REGISTER_HEAT_WT = 0x0E;
+static const uint8_t CG_ANEM_REGISTER_SUPPLY_V = 0x0D;
+
+static const uint8_t CG_ANEM_REGISTER_COLD_H = 0x10;
+static const uint8_t CG_ANEM_REGISTER_COLD_L = 0x11;
+static const uint8_t CG_ANEM_REGISTER_HOT_H = 0x12;
+static const uint8_t CG_ANEM_REGISTER_HOT_L = 0x13;
+static const uint8_t CG_ANEM_REGISTER_DT_H = 0x14;
+static const uint8_t CG_ANEM_REGISTER_DT_L = 0x15;
+
+static const uint8_t CG_ANEM_REGISTER_ADDRESS = 0x20;
+
+static const uint8_t CG_ANEM_REGISTER_WIND_MAX_H = 0x21;
+static const uint8_t CG_ANEM_REGISTER_WIND_MAX_L = 0x22;
+static const uint8_t CG_ANEM_REGISTER_WIND_MIN_H = 0x23;
+static const uint8_t CG_ANEM_REGISTER_WIND_MIN_L = 0x24;
+static const uint8_t CG_ANEM_REGISTER_RESET_WIND = 0x25;
+
+static const uint8_t CG_ANEM_STATUS_INCORRECT_TARING_RANGE = 0b10000000;
+static const uint8_t CG_ANEM_STATUS_INCORRECT_TARING = 0b01000000;
+static const uint8_t CG_ANEM_STATUS_WATCHDOG_TIMER = 0b00100000;
+static const uint8_t CG_ANEM_STATUS_OVERVOLTAGE = 0b00000010;
+static const uint8_t CG_ANEM_STATUS_UNSTEADY_PROCESS = 0b00000001;
+float version;
+inline uint16_t combine_bytes(uint8_t msb, uint8_t lsb) { return ((msb & 0xFF) << 8) | (lsb & 0xFF); }
+
+void CGAnemComponent::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up CG Anem...");
+
+
+  // Mark as not failed before initializing. Some devices will turn off sensors to save on batteries
+  // and when they come back on, the COMPONENT_STATE_FAILED bit must be unset on the component.
+  if ((this->component_state_ & COMPONENT_STATE_MASK) == COMPONENT_STATE_FAILED) {
+    this->component_state_ &= ~COMPONENT_STATE_MASK;
+    this->component_state_ |= COMPONENT_STATE_CONSTRUCTION;
+  }
+  uint8_t versionRaw = 0;
+  if (!this->read_byte(CG_ANEM_REGISTER_VERSION, &versionRaw)) {
+    this->error_code_ = COMMUNICATION_FAILED;
+    this->mark_failed();
+    return;
+  }
+  version = versionRaw / 10.0;
+  ESP_LOGI(TAG, "Version: %.1f", version);
+  if (this->firmware_version_sensor_!= nullptr)
+    this->firmware_version_sensor_->publish_state(version);
+  if (!this->write_byte(CG_ANEM_REGISTER_WHO_I_AM, 0x11)) {
+    this->error_code_ = COMMUNICATION_FAILED;
+    this->mark_failed();
+    return;
+  }
+}
+void CGAnemComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "CGANEM:");
+  LOG_I2C_DEVICE(this);
+  switch (this->error_code_) {
+    case COMMUNICATION_FAILED:
+      ESP_LOGE(TAG, "Communication with CG Anem failed!");
+      break;
+    case NONE:
+    default:
+      break;
+  }
+
+  LOG_UPDATE_INTERVAL(this);
+
+  LOG_SENSOR("  ", "Ambient temperature", this->ambient_temperature_sensor_);
+  LOG_SENSOR("  ", "Hotend_temperature", this->hotend_temperature_sensor_);
+  LOG_SENSOR("  ", "Heat power", this->heat_power_sensor_);
+  LOG_SENSOR("  ", "Air_consumption", this->air_consumption_sensor_);
+  LOG_SENSOR("  ", "Air flow rate", this->air_flow_rate_sensor_);
+  LOG_SENSOR("  ", "Min air flow rate", this->min_air_flow_rate_sensor_);
+  LOG_SENSOR("  ", "Max air flow rate", this->max_air_flow_rate_sensor_);
+
+}
+
+
+void CGAnemComponent::read_status() {
+  uint8_t status;
+  if (!this->read_byte(CG_ANEM_REGISTER_STATUS, &status)) {
+    ESP_LOGW(TAG, "Error reading status register.");
+    this->status_set_warning();
+    return;
+  }
+
+  if (status & CG_ANEM_STATUS_INCORRECT_TARING_RANGE) {
+    ESP_LOGW(TAG, "Incorrect taring range detected");
+  }
+
+  if (status & CG_ANEM_STATUS_INCORRECT_TARING) {
+    ESP_LOGW(TAG, "Incorrect taring detected");
+  }
+
+  if ((status & CG_ANEM_STATUS_WATCHDOG_TIMER) == 0) {
+    ESP_LOGW(TAG, "Watchdog disabled");
+    // this->status_wdt_sensor_->publish_state(true);
+  } else {
+    // this->status_wdt_sensor_->publish_state(false);
+  }
+
+
+  if (status & CG_ANEM_STATUS_OVERVOLTAGE) {
+    ESP_LOGW(TAG, "Overvoltage detected");
+    // this->status_ov_sensor_->publish_state(true);
+  } else {
+    // this->status_ov_sensor_->publish_state(false);
+  }
+
+  if (status & CG_ANEM_STATUS_UNSTEADY_PROCESS) {
+    ESP_LOGW(TAG, "Unsteady process detected");
+    this->status_set_warning();
+    // this->status_up_sensor_->publish_state(true);
+  } else {
+    this->status_clear_warning();
+    // this->status_up_sensor_->publish_state(false);
+  }
+}
+
+void CGAnemComponent::update() {
+  this->read_status();
+
+  if (this->status_has_warning()) {
+    return;
+  }
+
+  uint8_t tempH, tempL, hotH, hotL, speedH, speedL, MinAirH, MinAirL, MaxAirH, MaxAirL, PowerRaw, Supply_vRaw;
+  float hot, temp, speed, min_air, max_air, power;
+  
+
+  if (this-> read_byte(CG_ANEM_REGISTER_COLD_H, &tempH)) {
+    if (this-> read_byte(CG_ANEM_REGISTER_COLD_L, &tempL)) {
+      temp = ((tempH << 8) | tempL) / 10.0f;
+    } else {
+      ESP_LOGW(TAG, "Error reading cold temp-L.");
+      //this->status_set_warning();
+      return;
+    }
+  } else {
+    ESP_LOGW(TAG, "Error reading cold temp-H.");
+    this->status_set_warning();
+    return;
+  }
+  
+  if (this-> read_byte(CG_ANEM_REGISTER_HOT_H, &hotH)) {
+    if (this-> read_byte(CG_ANEM_REGISTER_HOT_L, &hotL)) {
+      hot = ((hotH << 8) | hotL) / 10.0f;
+    } else {
+      ESP_LOGW(TAG, "Error reading hot temp-L.");
+      //this->status_set_warning();
+      return;
+    }
+  } else {
+    ESP_LOGW(TAG, "Error reading hot temp-H.");
+    this->status_set_warning();
+    return;
+  }
+
+  if (this-> read_byte(CG_ANEM_REGISTER_WIND_H, &speedH)) {
+    if (this-> read_byte(CG_ANEM_REGISTER_WIND_L, &speedL)) {
+      speed = ((speedH << 8) | speedL) / 10.0f;
+
+    } else {
+      ESP_LOGW(TAG, "Error reading wind speed-L.");
+      this->status_set_warning();
+      return;
+    }
+  } else {
+    ESP_LOGW(TAG, "Error reading wind speed_H.");
+    this->status_set_warning();
+    return;
+  }
+  
+
+  if (version >= 1) {
+    if (this-> read_byte(CG_ANEM_REGISTER_WIND_MIN_H, &MinAirH)) {
+      if (this-> read_byte(CG_ANEM_REGISTER_WIND_MIN_L, &MinAirL)) {
+        min_air = ((MinAirH << 8) | MinAirL) / 10.0;
+        } else {
+          ESP_LOGW(TAG, "Error reading MinAir-L.");
+          this->status_set_warning();
+          return;
+        }
+      } else {
+        ESP_LOGW(TAG, "Error reading MinAir-H.");
+        this->status_set_warning();
+        return;
+      }
+    } else {
+      ESP_LOGW(TAG, "Error reading MinAir version < 1");
+      min_air = -1;
+    }
+
+  if (version >= 1) {
+    if (this->read_byte(CG_ANEM_REGISTER_WIND_MAX_H, &MaxAirH)) {
+      if (this->read_byte(CG_ANEM_REGISTER_WIND_MAX_L, &MaxAirL)) {
+        max_air = ((MaxAirH << 8) | MaxAirL) / 10.0;
+        } else {
+          ESP_LOGW(TAG, "Error reading MaxAir-L.");
+          this->status_set_warning();
+          return;
+        }
+      } else {
+        ESP_LOGW(TAG, "Error reading MaxAir-H.");
+        this->status_set_warning();
+        return;
+      }
+    } else {
+      ESP_LOGW(TAG, "Error reading MaxAir version < 1");
+      max_air = -1;
+    }
+
+  float supply_v;
+  if (this->read_byte(CG_ANEM_REGISTER_SUPPLY_V, &Supply_vRaw)) {
+    supply_v = Supply_vRaw/10.0;
+    ESP_LOGI(TAG, "Supply_v: %.1f", supply_v);
+  } else {
+    ESP_LOGW(TAG, "Error reading Supply_v.");
+    return;
+  }
+
+
+  if (this->read_byte(CG_ANEM_REGISTER_HEAT_WT, &PowerRaw)) {
+    if (version >= 1) {
+      power = PowerRaw/255.0*(4.7*4.7/8);
+     } else {
+       power = PowerRaw/10.0;
+     }
+  } else {
+    ESP_LOGW(TAG, "Error reading power.");
+    this->status_set_warning();
+    return;
+  }
+
+  float consumption;
+  if (duct_ > -0.01 && speed != -255) {
+    consumption = speed * duct_ * 0.36;
+  } else {
+    consumption = -255;
+  }
+
+      
+  if (this->heat_power_sensor_ != nullptr)
+    this->heat_power_sensor_->publish_state(power);
+  if (this->ambient_temperature_sensor_ != nullptr)
+    this->ambient_temperature_sensor_->publish_state(temp);
+  if (this->hotend_temperature_sensor_ != nullptr)
+    this->hotend_temperature_sensor_->publish_state(hot);
+  if (this->air_flow_rate_sensor_ != nullptr)
+    this->air_flow_rate_sensor_->publish_state(speed);
+  if (this->air_consumption_sensor_ != nullptr)
+    this->air_consumption_sensor_->publish_state(consumption);
+  if (this->min_air_flow_rate_sensor_ != nullptr)
+    this->min_air_flow_rate_sensor_->publish_state(min_air);
+  if (this->max_air_flow_rate_sensor_ != nullptr)
+    this->max_air_flow_rate_sensor_->publish_state(max_air);
+ 
+  ESP_LOGV(TAG, "Got temperature=%.1f°C speed=%.1fm/s ", temp, speed);
+
+
+
+}
+
+}  // namespace cg_anem
+}  // namespace esphome
---
+++ esphome/components/cg_anem/cg_anem.h
@@ -0,0 +1,52 @@
+#pragma once
+
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace cg_anem {
+
+/// This class implements support for the GC Anem Temperature+Pressure+Humidity i2c sensor.
+class CGAnemComponent : public PollingComponent, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void dump_config() override;
+  void update() override;
+
+  float get_setup_priority() const override { return setup_priority::DATA; }
+  void set_duct(float duct) { duct_ = duct; }
+  void set_ambient_temperature_sensor(sensor::Sensor *ambient_temperature_sensor) { this->ambient_temperature_sensor_ = ambient_temperature_sensor; }
+  void set_hotend_temperature_sensor(sensor::Sensor *hotend_temperature_sensor) { this->hotend_temperature_sensor_ = hotend_temperature_sensor; }
+  void set_heat_power_sensor(sensor::Sensor *heat_power_sensor) { this->heat_power_sensor_ = heat_power_sensor; }
+  void set_air_consumption_sensor(sensor::Sensor *air_consumption_sensor) { this->air_consumption_sensor_ = air_consumption_sensor; }
+  void set_air_flow_rate_sensor(sensor::Sensor *air_flow_rate_sensor) { this->air_flow_rate_sensor_ = air_flow_rate_sensor; }
+  void set_firmware_version_sensor(sensor::Sensor *firmware_version_sensor) { this->firmware_version_sensor_ = firmware_version_sensor; }
+  void set_min_air_flow_rate_sensor(sensor::Sensor *min_air_flow_rate_sensor) { this->min_air_flow_rate_sensor_ = min_air_flow_rate_sensor; }
+  void set_max_air_flow_rate_sensor(sensor::Sensor *max_air_flow_rate_sensor) { this->max_air_flow_rate_sensor_ = max_air_flow_rate_sensor; }
+  //void set_status_up_binary_sensor(binary_sensor::BinarySensor *status_up_binary_sensor) { this->status_up_binary_sensor_ = status_up_binary_sensor; }
+
+ protected:
+  sensor::Sensor *ambient_temperature_sensor_{nullptr};
+  sensor::Sensor *hotend_temperature_sensor_{nullptr};
+  sensor::Sensor *heat_power_sensor_{nullptr};
+  sensor::Sensor *air_consumption_sensor_{nullptr};
+  sensor::Sensor *air_flow_rate_sensor_{nullptr};
+  sensor::Sensor *firmware_version_sensor_{nullptr};
+  sensor::Sensor *min_air_flow_rate_sensor_{nullptr};
+  sensor::Sensor *max_air_flow_rate_sensor_{nullptr};
+
+  float duct_;
+
+  enum ErrorCode {
+    NONE = 0,
+    COMMUNICATION_FAILED,
+  } error_code_{NONE};
+
+ private:
+  void read_status();
+};
+
+}  // namespace cg_anem
+}  // namespace esphome
